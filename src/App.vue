64f884224844aff122032723f287352816d1cac0
rdv-omsk <rdv-omsk@mail.ru>
2022-05-27 13:16:43 +0300

    10-2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,44 @@
 <template>
-  <div class="container">
+  <div class="container pt-2">
+    <div class="form-group">
+      <label for="name">Car name</label>
+      <input type="text" id="name" class="form-control" v-model.trim="carName">
+    </div>
+
+    <div class="form-group">
+      <label for="year">Car year</label>
+      <input type="text" id="year" class="form-control" v-model.number="carYear">
+    </div>
+
+    <button class="btn btn-success" @click="createCar">Create car</button>
 
   </div>
 </template>
 
 
 <script>
-  export default {}
+  export default {
+    data () {
+      return {
+        carName: '',
+        carYear: 2018
+      }
+    },
+    methods: {
+      createCar () {
+        const car = {
+          name: this.carName,
+          year: this.carYear
+        }
+
+        this.$http.post('http://localhost:3000/cars', car)
+        .then(response => {
+          return response.json()
+        })
+        .then(newCar => {
+          console.log(newCar)
+        })
+      }
+    }
+  }
 </script>
