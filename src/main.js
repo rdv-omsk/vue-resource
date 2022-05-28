ed63bf38db737b202cdfe4052d13d303b1281885
rdv-omsk <rdv-omsk@mail.ru>
2022-05-28 15:50:47 +0300

    10-6

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ Vue.use(VueResource)
 
 Vue.http.options.root = 'http://localhost:3000/'
 
+Vue.http.interceptors.push(request => {
+  request.headers.set('Auth', 'RAND TOKEN ' + Math.random())
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
